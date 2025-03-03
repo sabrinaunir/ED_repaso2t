aa9b53088d0051493be0f0b5200ec982b75be46d
Sabrina <sabrina.aigbogun690@comunidadunir.net>
2025-03-03 14:14:10 +0100

    UPDATE: Clases biblioteca.java, Libro.java y Main.java

--- src/main/java/biblioteca/Biblioteca.java
+++ src/main/java/biblioteca/Biblioteca.java
@@ -1,4 +1,132 @@
-package Biblioteca;
+package biblioteca;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Clase que representa una biblioteca con una colección de libros
+ * Permite agregar, eliminar y buscar libros por título y por autor
+ *
+ * @see Libro
+ * @author Luis García
+ * @version 4.3.2025
+ */
 public class Biblioteca {
+
+    // TODO: Documentar estos atributos
+    /** Lista de libros disponibles en la biblioteca */
+    private final List<Libro> libros;
+
+    // TODO: Documentar este método
+    /** Constructor por defecto de la biblioteca sin libros */
+    public Biblioteca() {
+        libros = new ArrayList<>();
+    }
+
+    // TODO: Documentar este método.
+    //  Test: NO HAY QUE TESTEAR ESTE METODO
+    /** Constructor con parámetros.
+     *
+     * @param libros Lista de libros con la que se iniciliza la biblioteca
+     */
+    public Biblioteca(List<Libro> libros) {
+        this.libros = libros;
+    }
+
+    // TODO: Documentar este metodo
+    //  Test: Comprobar si se ha agregado
+    /**
+     * Agrega un libro a la biblioteca
+     * @param libro Un libro para agregar
+     *
+     * @return {@code true} si el libro se ha agregado correctamente o {@code false} si el libro NO se ha agregado
+     * correctamente
+     */
+    public boolean agregarLibro(Libro libro) {
+        return libros.add(libro);
+    }
+
+    // TODO: Testear este metodo.
+    //  Test: comprobar si se ha eliminado
+    public boolean eliminarLibro(Libro libro) {
+        return libros.remove(libro);
+    }
+
+    // TODO: Documentar este método
+
+    /**
+     * Devuelve la lista de libros de la biblioteca
+     * @return lista de libros de la biblioteca
+     */
+    public List<Libro> getLibros() {
+        return libros;
+    }
+
+    // TODO: Documentar este método.
+    //  Test 01: buscar libro existente y comprobar que lo localiza.
+    //  Test 02: buscar libro NO existente y comprobar que no lo localiza.
+
+    /**
+     *
+     * @param titulo
+     * @return
+     */
+    public Libro encuentraLibroPorTitulo(String titulo) {
+        for (Libro libro : libros) {
+            if (libro.getTitulo().equals(titulo)) {
+                return libro;
+            }
+        }
+        return null;
+    }
+
+    // TODO: Documentar este metodo.
+    //  No testearlo
+    // Este metodo está obsoleto. Hay que documentarlo como tal.
+    //  Recuerda: las anotaciones @deprecated y @link a la nueva
+    //  versión mejorada encuentraLibrosPorAutor(...)
+    //  En esta ocasión, NO TESTEAREMOS este metodo
+
+    /**
+     * @deprecated  Este metodo ha quedado obsoleto
+     * Se recomienda usar {@link #encuentraLibrosPorAutor(String)} en su lugar
+     * @param autor
+     * @return
+     */
+    @Deprecated
+    public Libro encuentaLibroPorAutor(String autor) {
+        for (Libro libro : libros) {
+            if (libro.getAutor().equals(autor)) {
+                return libro;
+            }
+        }
+        return null;
+    }
+
+    // TODO: Documentar este metodo
+    // Este metodo sustituye al metodo anterior. Está disponible desde la
+    //  versión 2.0. Hay que documentarlo teniéndolo en cuenta.
+    // TODO: Testear este metodo.
+    //  Test 01: Comprobar la lista de libros que devuelve para un autor existente.
+    //  Test 02: Comprobar la lista de libros que devuelve para un autor NO existente
+
+    /**
+     * Busca todos los libros de un autor determinado
+     * @since V2.0
+     * Sustituye al metodo {@link #encuentraLibroPorTitulo(String)}
+     * @param autor
+     * @return
+     */
+    public List<Libro> encuentraLibrosPorAutor(String autor) {
+        List<Libro> listaLibros = null;
+        for (Libro libro : libros) {
+            if (libro.getAutor().equals(autor)) {
+                if (listaLibros == null) {
+                    listaLibros = new ArrayList<>();
+                }
+                listaLibros.add(libro);
+            }
+        }
+        return listaLibros;
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,36 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import biblioteca.Libro;
+import biblioteca.Biblioteca;
+
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Biblioteca biblio = new Biblioteca();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Libro libro1 = new Libro("The Art of Computer Programming", "Donald Knuth", 1968);
+        biblio.agregarLibro(libro1);
+        System.out.println(libro1);
+
+        Libro libro2 = new Libro("Clean Code", "Robert C. Martin ", 2009);
+        biblio.agregarLibro(libro2);
+        System.out.println(libro2);
+
+        Libro libro3 = new Libro("Clean Arquitecture", "Robert C. Martin", 1968);
+        System.out.println(biblio.agregarLibro(libro3));
+        System.out.println(libro3);
+
+        System.out.println(biblio.encuentaLibroPorAutor("Robert C. Martin"));
+        System.out.println(biblio.encuentraLibrosPorAutor("Robert C. Martin"));
+
+        System.out.println(biblio.getLibros());
+        System.out.println(biblio.eliminarLibro(libro1));
+        System.out.println(biblio.getLibros());
+
+        System.out.println(biblio.encuentraLibroPorTitulo("Clean Code"));
+
+        // Copia de la biblioteca
+        Biblioteca biblio2 = new Biblioteca(biblio.getLibros());
+        System.out.println(biblio2.getLibros());
     }
 }
